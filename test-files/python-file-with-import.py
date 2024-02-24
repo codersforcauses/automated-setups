738ea563aae1f470b37535f92fde7de15b161303
Frinze Erin Lapuz <frinzekt@gmail.com>
2024-02-25 00:24:58 +0800

    NONE Added test files to test import detection

--- test-files/python-file-with-import.py
+++ test-files/python-file-with-import.py
@@ -1,5 +1,8 @@
-import django
-import os
+# Sampling a dependency installed in poetry
+# making sure python detects the dependency for intellicode
+import sqlparse
 
 def test_function():
     print('This is a test function')
+    sqlparse.format('SELECT * FROM table', reindent=True, keyword_case='upper')
+    
