7de5fdad84615109fc902a6eb1b0145297d65a1a
Frinze Erin Lapuz <frinzekt@gmail.com>
2024-02-24 23:13:55 +0800

    NONE Change the messaging in impact

--- cfc-devenv-base/utils/startup_display.sh
+++ cfc-devenv-base/utils/startup_display.sh
@@ -2,5 +2,5 @@
 
 figlet 'CFC - Devenv'
 echo 'Made possible by CFC Tech Lead 2021 - Frinze Lapuz (CFC commitee) with a passion to automate'
-echo 'your setup so you can focus more on what matters - building your project'
+echo 'your setup so you can focus more on what matters - building features that impact the community'
 echo 'Read more about this setup at https://github.com/codersforcauses/automated-setups'
